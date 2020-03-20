f334f7b5e7c12b8dd5830c7197b3b79d8a70e3b8
Ankur Kumar Rai <rai.ankur0007@gmail.com>
2020-03-20 15:27:41 +0530

    added interpolatin and function call to  component

--- src/app/first-component/first-component.component.ts
+++ src/app/first-component/first-component.component.ts
@@ -6,10 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./first-component.component.css']
 })
 export class FirstComponentComponent implements OnInit {
-
+  i = 0;
+  someVariable = Math.random();
   constructor() { }
-
   ngOnInit(): void {
+    // like when we add a time interval function it will call it so many times
+    // if any variable is changed
+    // uncomment the following code to see effect
+  
+    // setInterval(() => {
+    //   this.someVariable = Math.random();
+    // }, 1000);
+  }
+  // we should not use some function call in interpolateion because it will
+  // call every time when view will refresh
+
+  doSomeTask(){
+    console.log(`Called ${this.i++} times`)
   }
 
 }
