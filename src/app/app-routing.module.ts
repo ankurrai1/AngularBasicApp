7e01fa1b705122aa7d78b381eaa1437b973984e9
Ankur Kumar Rai <rai.ankur0007@gmail.com>
2020-03-26 13:05:45 +0530

    added example to app of predefined pipings

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+
 
 import { HomeComponent } from './home/home.component';
 import { DataComponent } from './data/data.component';
 import { LoginComponent } from './login/login.component';
 import { AdminComponent } from './admin/admin.component';
+import { PipeExampleComponent } from "./pipe-example/pipe-example.component";
 
 import { AuthGuard } from "./auth.guard";
 
 
 const routes: Routes = [
+
   {
     path:"",
     component:HomeComponent
@@ -21,6 +25,10 @@ const routes: Routes = [
   {
     path:"login",
     component:LoginComponent
+  },
+  {
+    path:"pipe",
+    component:PipeExampleComponent,
   },
   {
     path:"admin",
