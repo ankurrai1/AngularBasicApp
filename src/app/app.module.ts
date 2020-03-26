41b1b782f023b75c6d59c8cac65a1e5146d4e777
Ankur Kumar Rai <rai.ankur0007@gmail.com>
2020-03-26 12:37:55 +0530

    added new component to show case pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import { LoginComponent } from './login/login.component';
 import { AdminComponent } from './admin/admin.component';
 import { AuthGuard } from "./auth.guard";
 import { AuthService } from "./auth.service";
+import { PipeExampleComponent } from './pipe-example/pipe-example.component';
 
 @NgModule({
   declarations: [
@@ -20,7 +21,8 @@ import { AuthService } from "./auth.service";
     HomeComponent,
     DataComponent,
     LoginComponent,
-    AdminComponent
+    AdminComponent,
+    PipeExampleComponent
   ],
   imports: [
     BrowserModule,
