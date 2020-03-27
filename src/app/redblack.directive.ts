18342698cc15fe9263c8f4e7ef48d444ddad72d8
Ankur Kumar Rai <rai.ankur0007@gmail.com>
2020-03-27 23:37:18 +0530

    added custom directives redblack to app

--- src/app/redblack.directive.ts
+++ src/app/redblack.directive.ts
@@ -1,10 +1,13 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef } from '@angular/core';
 
 @Directive({
   selector: '[appRedblack]'
 })
 export class RedblackDirective {
 
-  constructor() { }
+  constructor(private el:ElementRef) {
+    el.nativeElement.style.color = "red"
+    el.nativeElement.style.backgroundColor = "yellow "
+  }
 
 }
