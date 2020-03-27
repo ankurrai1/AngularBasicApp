445140608ea0f0d3cd3ced74f6f3e6f0fb82a9fe
Ankur Kumar Rai <rai.ankur0007@gmail.com>
2020-03-27 23:19:24 +0530

    added *ngif directive to home work

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,10 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
+  public SomeVariable = true
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  toggelVariable(){
+    this.SomeVariable = !this.SomeVariable;
   }
 
 }
